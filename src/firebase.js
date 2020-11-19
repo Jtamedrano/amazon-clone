3b2dc01b9010e24906b81911f3cace96e60d3f67
Jesse Medrano <Jtamedrano@gmail.com>
2020-11-19 11:17:35 -0800

    Connection to firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAA7jiXq2CfUNXP-2ZR3X4YqdEwzJ4zDiU",
+  authDomain: "clone-7b00a.firebaseapp.com",
+  databaseURL: "https://clone-7b00a.firebaseio.com",
+  projectId: "clone-7b00a",
+  storageBucket: "clone-7b00a.appspot.com",
+  messagingSenderId: "43644434640",
+  appId: "1:43644434640:web:2372e804ab573a578c06d5",
+  measurementId: "G-7F2JD71G3P",
+};
