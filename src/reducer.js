1af495cfdd61b9f0faad02bbef7f9c3e344c6503
Jesse Medrano <Jtamedrano@gmail.com>
2020-11-19 08:58:19 -0800

    Added "REMOVED_FROM_BASKET" function

--- src/reducer.js
+++ src/reducer.js
@@ -10,9 +10,22 @@ function reducer(state, action) {
     case "ADD_TO_BASKET":
       // LOGIC FOR ADDING TO BASKET
       return { ...state, basket: [...state.basket, action.payload] };
-    case "REMOVE_FROM_BASKET":
+    case "REMOVED_FROM_BASKET":
       // LOGIC FOR ADDING TO BASKET
-      return state;
+      const index = state.basket.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cannot remove basket item (id: ${action.payload.id}) as it is not in the basket.`
+        );
+      }
+      return { ...state, basket: newBasket };
     default:
       return state;
   }
